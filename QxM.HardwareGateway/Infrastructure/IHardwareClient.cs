4de328ff4986eb3d5ca20bf515eccd5168fc3ae2
glc <gabriel.lopez.candales@gmail.com>
2026-01-14 09:40:51 -0800

    [QxM]: fixing potential runtime bug and abstracted things away to make it cleaner. In particular made IHardwareClient.cs generic with a TRequest as a HardwareCommandRequest so client implementations can use their specific request type.

--- QxM.HardwareGateway/Infrastructure/IHardwareClient.cs
+++ QxM.HardwareGateway/Infrastructure/IHardwareClient.cs
@@ -5,7 +5,7 @@ using QxM.HardwareGateway.Core.State;
 
 namespace QxM.HardwareGateway.Infrastructure;
 
-public interface IHardwareClient
+public interface IHardwareClient<TRequest> where TRequest : HardwareCommandRequest
 {
     HardwareId HardwareId { get; }
     HardwareKind HardwareKind { get; }
@@ -30,7 +30,7 @@ public interface IHardwareClient
     /// <param name="request"></param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    Task<HardwareCommandResponse> ExecuteCommandAsync(HardwareCommandRequest request, CancellationToken cancellationToken = default);
+    Task<HardwareCommandResponse> ExecuteCommandAsync(TRequest request, CancellationToken cancellationToken = default);
     
     /// <summary>
     /// Submits a command and returns quickly after device accepts.
@@ -38,7 +38,7 @@ public interface IHardwareClient
     /// <param name="request"></param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    Task<HardwareCommandAccepted> SubmitCommandAsync(HardwareCommandRequest request, CancellationToken cancellationToken = default);
+    Task<HardwareCommandAccepted> SubmitCommandAsync(TRequest request, CancellationToken cancellationToken = default);
     
     /// <summary>
     /// Subscribes to all device events
--- QxM.HardwareGateway/Infrastructure/Simulators/SimulatedIcbClient.cs
+++ QxM.HardwareGateway/Infrastructure/Simulators/SimulatedIcbClient.cs
@@ -9,7 +9,7 @@ using QxM.HardwareGateway.Core.Utilities;
 
 namespace QxM.HardwareGateway.Infrastructure.Simulators;
 
-public sealed class SimulatedIcbClient : IHardwareClient, IAsyncDisposable
+public sealed class SimulatedIcbClient : IHardwareClient<CanFrameCommandRequest>, IAsyncDisposable
 {
     private readonly FiniteStateMachine<ConnectionState, ConnectionTrigger> _fsm;
     private readonly Channel<HardwareEvent> _eventsChannel;
@@ -176,12 +176,9 @@ public sealed class SimulatedIcbClient : IHardwareClient, IAsyncDisposable
     /// <param name="request"></param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    public async Task<HardwareCommandResponse> ExecuteCommandAsync(HardwareCommandRequest request, CancellationToken cancellationToken = default)
+    public async Task<HardwareCommandResponse> ExecuteCommandAsync(CanFrameCommandRequest request, CancellationToken cancellationToken = default)
     {
         EnsureConnected();
-
-        // set as a CAN Frame Command Request
-        request = ((CanFrameCommandRequest)request);
 
         _completed.TryAdd(request.Id, 
             new TaskCompletionSource<HardwareCommandResponse>(TaskCreationOptions.RunContinuationsAsynchronously));
@@ -234,13 +231,10 @@ public sealed class SimulatedIcbClient : IHardwareClient, IAsyncDisposable
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
     /// <exception cref="NotImplementedException"></exception>
-    public async Task<HardwareCommandAccepted> SubmitCommandAsync(HardwareCommandRequest request, CancellationToken cancellationToken = default)
+    public async Task<HardwareCommandAccepted> SubmitCommandAsync(CanFrameCommandRequest request, CancellationToken cancellationToken = default)
     {
         EnsureConnected();
         CleanupIdempotency();
-        
-        // set as a CAN Frame Command Request
-        request = ((CanFrameCommandRequest)request);
         
         using var cts = new CancellationTokenSource(TimeoutPolicy.CommandTimeout);
         using var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, cts.Token);
