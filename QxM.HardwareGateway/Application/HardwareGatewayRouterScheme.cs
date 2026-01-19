41b728d4b7910cb2b6556a0b321a8d9f3ede34d3
glc <gabriel.lopez.candales@gmail.com>
2026-01-18 17:54:51 -0800

    [QxM-19]: Setup the HardwareGatewayRouterScheme.cs as an immutable record

---
+++ QxM.HardwareGateway/Application/HardwareGatewayRouterScheme.cs
@@ -0,0 +1,5 @@
+using QxM.HardwareGateway.Core;
+
+namespace QxM.HardwareGateway.Application;
+
+public readonly record struct HardwareGatewayRouterScheme(HardwareId HardwareId, HardwareKind HardwareKind);
